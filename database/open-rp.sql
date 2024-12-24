776009aa5d51bd407594fb0d4d400e2129194a30
Breno Pereira <brenopereira856@gmail.com>
2024-12-24 15:24:16 -0300

    feat: update openMP and fix database syntax to import

--- database/open-rp.sql
+++ database/open-rp.sql
@@ -5,12 +5,12 @@ CREATE TABLE `users`(
     `Admin` int(6) DEFAULT 0,
     `Money` int(12) DEFAULT 500,
     `Level` int(6) DEFAULT 0,
-    `Exp` int (12) DEFAULT 0,
+    `Exp` int(12) DEFAULT 0,
     `Skin` int(4) DEFAULT 60,
     `PosX` float DEFAULT -2240.9197,
     `PosY` float DEFAULT 252.0263,
     `PosZ` float DEFAULT 35.3203,
-    `PosA` float DEFAULT 91.2125
+    `PosA` float DEFAULT 91.2125,
     `JobID` int(4) DEFAULT 0,
     `JobXP` int(4) DEFAULT 0,
     `JobLVL` int(4) DEFAULT 0
